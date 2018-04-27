1f6b20f33dd446bae9a82e11b7529aa27a11484a
tayurus <tayurus@gmail.com>
2018-04-28 00:19:57 +0300

    add clients

---
+++ blocks/clients/clients.js
@@ -0,0 +1,16 @@
+$(".clients-carousel").owlCarousel({
+    items:1,
+    nav:true,
+    dots: false,
+    responsive:{
+        700:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
